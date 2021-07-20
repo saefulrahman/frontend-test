785673bab756480375a2003079516bf3e33c6773
Saeful Rahman <saeful13.id@gmail.com>
2021-07-20 14:39:28 +0700

    Update templating

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "reactstrap";
+import "assets/scss/style.scss";
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Button color="primary">primary</Button>
     </div>
   );
 }
