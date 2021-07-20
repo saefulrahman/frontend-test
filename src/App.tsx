6cb1c4bb4f974e51d9893bd2e7290ef67ea59a33
Saeful Rahman <saeful13.id@gmail.com>
2021-07-20 16:26:07 +0700

    Fixing routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Button } from "reactstrap";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+// import { render } from "react-dom";
+
 import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-      <Button color="primary">primary</Button>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
