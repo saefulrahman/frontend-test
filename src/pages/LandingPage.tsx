a244b94fe6a643827360724bd8fd48ddf8a7aed1
Saeful Rahman <saeful13.id@gmail.com>
2021-07-20 16:00:04 +0700

    Update typescript

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class LandingPage extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <h1>Hello World!</h1>
+      </>
+    );
+  }
+}
